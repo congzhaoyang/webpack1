3578449335f91c3321dd4e5c43b1ba76760092ff
congzhaoyang <congzhaoyang1@163.com>
2017-10-26 00:53:21 +0800

    [update] 引入app.bundle.js和print.bundle.js成功

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash'
+import printMe from './print'
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button')
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  // Add the image to our existing div.
-
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  
+  element.appendChild(btn);
+  
   return element;
 }
 
