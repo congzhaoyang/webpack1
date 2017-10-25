edeea97b80cb6e3910f0a037b5725d22af1cb3fc
congzhaoyang <congzhaoyang1@163.com>
2017-10-26 01:33:27 +0800

    [update] 加入webpack-dev-server和webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     // new HtmlWebpackPlugin({
     //   title: 'Output Management'
@@ -13,7 +17,8 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
